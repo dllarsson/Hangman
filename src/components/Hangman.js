87cb9ddec532fac237a1d2e8197297ac98d2ec65
David <david.l.larsson.90@gmail.com>
2020-06-11 11:44:20 +0200

    added play again button

--- src/components/Hangman.js
+++ src/components/Hangman.js
@@ -18,31 +18,45 @@ const Hangman = () => {
     const [guessedLetters, setLetters] = useState([])
     const [currentWord, setWord] = useState(word())
     const [isDisabled, setDisable] = useState([])
-    const [numberOfGuesses, setGuesses] = useState(0)
+    const [numberOfGuesses, setGuesses] = useState(6)
     const [currentPicture, setPicture] = useState([Gallow0])
+    const [hasWon, setWon] = useState(false)
+    const [count, setCount] = useState(0)
+    const [hasLost, setLost] = useState(false)
 
     const Gallows = [Gallow0,Gallow1,Gallow2,Gallow3,Gallow4,Gallow5,Gallow6]
+    const uniqueLetters = Array.from(new Set(currentWord.split('')))
 
     useEffect(() => {
-        setPicture(Gallows[numberOfGuesses])
-    }, [numberOfGuesses])
+        setPicture(Gallows[count])
+        setCount(count + 1)
+        numberOfGuesses === 0 && setLost(true)
 
-    const printWord = currentWord.split('').map(ltr => {
+    }, [numberOfGuesses])
+    useEffect(() => {
+        guessedLetters.length === uniqueLetters.length && setWon(true)
+
+    }, [guessedLetters])
+    
+
+    const printWord = currentWord.split('').map((ltr, index) => {
+        
         return(
             <Letter 
-                key={ltr} 
+                key={index} 
                 className="letter"
                 letter={guessedLetters.includes(ltr) ? ltr : " _ " } 
             />
         )
     })
-    
 
     const guessLetter = (e) => {
-        setLetters(guessedLetters.concat(e.target.innerText))
+        if (currentWord.includes(e.target.innerText)){
+            setLetters(guessedLetters.concat(e.target.innerText))
+        } else {
+            setGuesses(numberOfGuesses - 1)
+        }
         setDisable(isDisabled.concat(e.target.innerText))
-        setGuesses(numberOfGuesses + 1)
-        //guessedLetters.concat(e.target.innerText) || setPicture(ga)
         }
     const buttons = lettersArray.map((letter) => {
         return(
@@ -58,9 +72,12 @@ const Hangman = () => {
      
     const reset = () => {
        setDisable([])
-       setGuesses(0)
+       setGuesses(6)
        setLetters([])
        setWord(word())
+       setWon(false)
+       setLost(false)
+       setCount(0)
     }
     
     
@@ -72,10 +89,11 @@ const Hangman = () => {
             <div id="wordContainer">
                 {printWord}
             </div>
-            {numberOfGuesses > 5 && <p style={{fontSize: "40px"}}>Du förlorade</p>}
+            {hasLost && <p style={{fontSize: "40px"}}>Du förlorade, ditt ord var: {currentWord}</p>}
+            {hasWon && <p style={{fontSize: "40px", color: "blue"}}>Grattis du vann!</p>}
             <p>Antal gissningar: {numberOfGuesses}</p>
             <div className="buttonContainer">
-                {buttons}
+                {hasWon || hasLost ? <Button handleClick={reset} buttonText="Spela igen!" className="letterButton"/> : buttons}
             </div>
             <button onClick={reset}>Återställ</button>
         </div>
